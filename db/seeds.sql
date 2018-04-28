53e511b306bb668548654639fa4e3e4070793022
Abe Rivera <arevireba@gmail.com>
2018-04-28 14:09:56 -0700

    init commit

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+### Seeds
+INSERT INTO burgers (burger_name, devoured) VALUES ("Single Hamburger", False);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Hamburger", False);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Single Cheeseburger", True);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheeseburger", True);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger with Green Chile", True);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheeseburger with Green Chile", True);
