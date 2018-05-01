c558136a13b21fdeb46402a2fb0b6d4d3ef67f39
Abe Rivera <arevireba@gmail.com>
2018-04-30 18:25:06 -0700

    hellz no

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 // Require mysql
 var mysql = require("mysql");
 
-// Set up our connection information
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burgers_db"
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
+  }else{
+    connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "burgers_db"
   });
+};
 
 // Connect to the database
 connection.connect(function(err) {
